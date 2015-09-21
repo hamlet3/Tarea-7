81aef8a796da1838973a0d9afe4135c19cdd9a9e
hamlet3 <hamletreyes3@gmail.com>
2015-09-21 10:27:16 -0600

    Tarea

---
+++ Tarea ejercicio cap6/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Tarea_ejercicio_cap6
+{
+    partial class Tarea6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcTarea6 = new System.Windows.Forms.TabControl();
+            this.tpPromedio = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelMenor = new System.Windows.Forms.Label();
+            this.labelMayor = new System.Windows.Forms.Label();
+            this.txtNotas = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.labelTexto2 = new System.Windows.Forms.Label();
+            this.labelPromedio = new System.Windows.Forms.Label();
+            this.btnIngresar2 = new System.Windows.Forms.Button();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.labelTexto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEstudiantes = new System.Windows.Forms.TextBox();
+            this.txtAulas = new System.Windows.Forms.TextBox();
+            this.tabjagged = new System.Windows.Forms.TabPage();
+            this.tcTarea6.SuspendLayout();
+            this.tpPromedio.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tcTarea6
+            // 
+            this.tcTarea6.Controls.Add(this.tpPromedio);
+            this.tcTarea6.Controls.Add(this.tabjagged);
+            this.tcTarea6.Location = new System.Drawing.Point(-4, -1);
+            this.tcTarea6.Name = "tcTarea6";
+            this.tcTarea6.SelectedIndex = 0;
+            this.tcTarea6.Size = new System.Drawing.Size(325, 328);
+            this.tcTarea6.TabIndex = 0;
+            // 
+            // tpPromedio
+            // 
+            this.tpPromedio.Controls.Add(this.label4);
+            this.tpPromedio.Controls.Add(this.label3);
+            this.tpPromedio.Controls.Add(this.label2);
+            this.tpPromedio.Controls.Add(this.labelMenor);
+            this.tpPromedio.Controls.Add(this.labelMayor);
+            this.tpPromedio.Controls.Add(this.txtNotas);
+            this.tpPromedio.Controls.Add(this.btnProcesar);
+            this.tpPromedio.Controls.Add(this.labelTexto2);
+            this.tpPromedio.Controls.Add(this.labelPromedio);
+            this.tpPromedio.Controls.Add(this.btnIngresar2);
+            this.tpPromedio.Controls.Add(this.btnIngresar);
+            this.tpPromedio.Controls.Add(this.labelTexto);
+            this.tpPromedio.Controls.Add(this.label1);
+            this.tpPromedio.Controls.Add(this.txtEstudiantes);
+            this.tpPromedio.Controls.Add(this.txtAulas);
+            this.tpPromedio.Location = new System.Drawing.Point(4, 22);
+            this.tpPromedio.Name = "tpPromedio";
+            this.tpPromedio.Padding = new System.Windows.Forms.Padding(3);
+            this.tpPromedio.Size = new System.Drawing.Size(317, 302);
+            this.tpPromedio.TabIndex = 0;
+            this.tpPromedio.Text = "Promedio";
+            this.tpPromedio.UseVisualStyleBackColor = true;
+            this.tpPromedio.Click += new System.EventHandler(this.tpPromedio_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Promedio de notas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nota mayor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nota menor";
+            // 
+            // labelMenor
+            // 
+            this.labelMenor.AutoSize = true;
+            this.labelMenor.Location = new System.Drawing.Point(215, 228);
+            this.labelMenor.Name = "labelMenor";
+            this.labelMenor.Size = new System.Drawing.Size(0, 13);
+            this.labelMenor.TabIndex = 11;
+            // 
+            // labelMayor
+            // 
+            this.labelMayor.AutoSize = true;
+            this.labelMayor.Location = new System.Drawing.Point(216, 55);
+            this.labelMayor.Name = "labelMayor";
+            this.labelMayor.Size = new System.Drawing.Size(0, 13);
+            this.labelMayor.TabIndex = 10;
+            // 
+            // txtNotas
+            // 
+            this.txtNotas.Location = new System.Drawing.Point(12, 151);
+            this.txtNotas.Name = "txtNotas";
+            this.txtNotas.Size = new System.Drawing.Size(56, 20);
+            this.txtNotas.TabIndex = 9;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(10, 188);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(58, 23);
+            this.btnProcesar.TabIndex = 8;
+            this.btnProcesar.Text = "procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click_1);
+            // 
+            // labelTexto2
+            // 
+            this.labelTexto2.AutoSize = true;
+            this.labelTexto2.Location = new System.Drawing.Point(13, 132);
+            this.labelTexto2.Name = "labelTexto2";
+            this.labelTexto2.Size = new System.Drawing.Size(35, 13);
+            this.labelTexto2.TabIndex = 7;
+            this.labelTexto2.Text = "Notas";
+            // 
+            // labelPromedio
+            // 
+            this.labelPromedio.AutoSize = true;
+            this.labelPromedio.Location = new System.Drawing.Point(215, 140);
+            this.labelPromedio.Name = "labelPromedio";
+            this.labelPromedio.Size = new System.Drawing.Size(0, 13);
+            this.labelPromedio.TabIndex = 6;
+            // 
+            // btnIngresar2
+            // 
+            this.btnIngresar2.Location = new System.Drawing.Point(74, 76);
+            this.btnIngresar2.Name = "btnIngresar2";
+            this.btnIngresar2.Size = new System.Drawing.Size(58, 23);
+            this.btnIngresar2.TabIndex = 5;
+            this.btnIngresar2.Text = "Ingresar";
+            this.btnIngresar2.UseVisualStyleBackColor = true;
+            this.btnIngresar2.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(10, 75);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(58, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // labelTexto
+            // 
+            this.labelTexto.AutoSize = true;
+            this.labelTexto.Location = new System.Drawing.Point(71, 16);
+            this.labelTexto.Name = "labelTexto";
+            this.labelTexto.Size = new System.Drawing.Size(62, 13);
+            this.labelTexto.TabIndex = 3;
+            this.labelTexto.Text = "Estudiantes";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Aulas";
+            // 
+            // txtEstudiantes
+            // 
+            this.txtEstudiantes.Location = new System.Drawing.Point(74, 40);
+            this.txtEstudiantes.Name = "txtEstudiantes";
+            this.txtEstudiantes.Size = new System.Drawing.Size(58, 20);
+            this.txtEstudiantes.TabIndex = 1;
+            // 
+            // txtAulas
+            // 
+            this.txtAulas.Location = new System.Drawing.Point(12, 40);
+            this.txtAulas.Name = "txtAulas";
+            this.txtAulas.Size = new System.Drawing.Size(56, 20);
+            this.txtAulas.TabIndex = 0;
+            // 
+            // tabjagged
+            // 
+            this.tabjagged.Location = new System.Drawing.Point(4, 22);
+            this.tabjagged.Name = "tabjagged";
+            this.tabjagged.Padding = new System.Windows.Forms.Padding(3);
+            this.tabjagged.Size = new System.Drawing.Size(317, 302);
+            this.tabjagged.TabIndex = 1;
+            this.tabjagged.Text = "tab";
+            this.tabjagged.UseVisualStyleBackColor = true;
+            // 
+            // Tarea6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 322);
+            this.Controls.Add(this.tcTarea6);
+            this.Name = "Tarea6";
+            this.Text = "Tarea6";
+            this.tcTarea6.ResumeLayout(false);
+            this.tpPromedio.ResumeLayout(false);
+            this.tpPromedio.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tcTarea6;
+        private System.Windows.Forms.TabPage tpPromedio;
+        private System.Windows.Forms.TabPage tabjagged;
+        private System.Windows.Forms.Button btnIngresar2;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label labelTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEstudiantes;
+        private System.Windows.Forms.TextBox txtAulas;
+        private System.Windows.Forms.Label labelTexto2;
+        private System.Windows.Forms.Label labelPromedio;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.TextBox txtNotas;
+        private System.Windows.Forms.Label labelMenor;
+        private System.Windows.Forms.Label labelMayor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tarea ejercicio cap6/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_ejercicio_cap6
+{
+    public partial class Tarea6 : Form
+    {
+        double[][] calificaciones;
+        int aulas = 0, estudiantes = 0;
+        int n = 0,x=0,y=0;
+        double resultado = 0;
+        int contador = 0;
+        public Tarea6()
+        {
+            InitializeComponent();
+        }
+
+        public double NotaMenor(double[][] calificaciones) {
+            double menor = 999;
+            for(int j = 0; j < aulas; j++) {
+                for (int k = 0; k < calificaciones[j].GetLength(0);k++) {
+                    if (calificaciones[j][k]< menor)
+                    {
+                        menor = calificaciones[j][k];
+                    }
+
+                }
+            }
+
+           
+            return menor;
+        }
+
+        public double NotaMayor(double[][] calificaciones)
+        {
+            double mayor = -999;
+            for (int j = 0; j < aulas; j++)
+            {
+                for (int k = 0; k < calificaciones[j].GetLength(0); k++)
+                {
+                    if (calificaciones[j][k] > mayor)
+                    {
+                        mayor = calificaciones[j][k];
+                    }
+                }
+            }
+
+            return mayor;
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+             
+            estudiantes = int.Parse(txtEstudiantes.Text);
+            if (n <aulas)
+            {
+                calificaciones[n] = new double[estudiantes];
+                n++;
+                contador += estudiantes;
+            }
+            labelTexto.Text = "aula no#: " + n;
+        }
+
+        private void tpPromedio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProcesar_Click_1(object sender, EventArgs e)
+        {
+
+            if (x <=aulas-1)
+            {
+                labelTexto2.Text = "estudiante no#" + y + "Del aula no#" + x;
+                if (y < calificaciones[x].GetLength(0))
+                {
+                    calificaciones[x][y] = double.Parse(txtNotas.Text);
+                    y++;
+                    resultado += double.Parse(txtNotas.Text);
+                    txtNotas.Clear();
+                    txtNotas.Focus();
+                }
+                else
+                {
+                    x++;
+                    if (x<2) {
+                        
+                        y = 0;
+                        calificaciones[x][y] = double.Parse(txtNotas.Text);
+                        y++;
+                        resultado += double.Parse(txtNotas.Text);
+                        txtNotas.Clear();
+                        txtNotas.Focus();
+
+                    }
+
+                }
+                
+            }
+            else
+            {
+                resultado /= contador;
+                labelPromedio.Text = resultado.ToString();
+                resultado = NotaMenor(calificaciones);
+                labelMenor.Text = resultado.ToString();
+                resultado = NotaMayor(calificaciones);
+                labelMayor.Text = resultado.ToString();
+                btnProcesar.Enabled=false;
+            }
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            aulas = int.Parse(txtAulas.Text);
+            calificaciones = new double[aulas][];
+        }
+        
+    }
+}
